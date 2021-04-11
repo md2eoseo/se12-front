589269b7c5805a8d7d8b83641b14c483ff7570e9
md2eoseo <md2eoseo@gmail.com>
2021-04-11 12:15:16 +0900

    Login function component

--- src/routes/Login.js
+++ src/routes/Login.js
@@ -1,69 +1,36 @@
-import React from 'react';
+import { useState } from 'react';
 import './css/Login.css';
 
-class Login extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      email: '',
-      password: '',
-      idChecked: false,
-      pwChecked: false,
-      btnColor: '#4374D9',
-    };
-  }
+function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-  onSubmit = e => {
+  const onSubmit = e => {
     e.preventDefault();
-    console.log('사용자 Email :', this.state.email);
-    console.log('사용자 Password :', this.state.password);
+    console.log('사용자 Email :', email);
+    console.log('사용자 Password :', password);
   };
 
-  emailInputCheck = e => {
-    this.setState({ email: e.target.value });
-    if (e.target.value.includes('@')) {
-      this.setState({ idChecked: true }, () => this.btnChangeColor());
-    } else {
-      this.setState({ idChecked: false }, () => this.btnChangeColor());
-    }
+  const onEmailChange = e => {
+    setEmail(e.target.value);
   };
 
-  pwInputCheck = e => {
-    this.setState({ password: e.target.value });
-    if (e.target.value.length >= 5) {
-      this.setState({ userName: e.target.value, pwChecked: true }, () => this.btnChangeColor());
-    } else {
-      this.setState({ pwChecked: false }, () => this.btnChangeColor());
-    }
+  const onPasswordChange = e => {
+    setPassword(e.target.value);
   };
 
-  btnChangeColor = () => {
-    if (this.state.idChecked && this.state.pwChecked) {
-      this.setState({ btnColor: '#4374D9' });
-    } else {
-      this.setState({ btnColor: '#FF5A5A' });
-    }
-  };
-
-  render() {
-    return (
-      <div className="Login">
-        <div className="title">로그인</div>
-        <form className="form" onSubmit={this.onSubmit}>
-          <input className="email" type="text" placeholder="이메일" onChange={this.emailInputCheck} />
-          <input className="password" type="password" placeholder="비밀번호 (5자 이상)" onChange={this.pwInputCheck} />
-          <button
-            className="submitBtn"
-            type="submit"
-            disabled={!this.state.idChecked || !this.state.pwChecked}
-            style={{ backgroundColor: this.state.btnColor }}
-          >
-            로그인
-          </button>
-        </form>
-      </div>
-    );
-  }
+  return (
+    <div className="Login">
+      <div className="title">로그인</div>
+      <form className="form" onSubmit={onSubmit}>
+        <input className="email" type="text" placeholder="이메일" onChange={onEmailChange} />
+        <input className="password" type="password" placeholder="비밀번호 (5자 이상)" onChange={onPasswordChange} />
+        <button className="submitBtn" type="submit">
+          로그인
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default Login;
