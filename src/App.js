a4b19b115b0325c8c0d7200f04e40c467103d179
ryool <fbfbf1@naver.com>
2021-04-11 12:04:38 +0900

    ryool implementation code

--- src/App.js
+++ src/App.js
@@ -1,41 +1,32 @@
+import React, { Component } from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>소프트웨어공학 12팀</h1>
-        <table>
-          <tr>
-            <th>학번</th>
-            <th>이름</th>
-          </tr>
-          <tr>
-            <td>15109316</td>
-            <td>김성태</td>
-          </tr>
-          <tr>
-            <td>16102203</td>
-            <td>위성률</td>
-          </tr>
-          <tr>
-            <td>18101229</td>
-            <td>신현규</td>
-          </tr>
-          <tr>
-            <td>18101281</td>
-            <td>최해솔</td>
-          </tr>
-        </table>
-        <a className="App-link" href="https://github.com/md2eoseo/se12-front" target="_blank" rel="noopener noreferrer">
-          Frontend
-        </a>
-        <a className="App-link" href="https://github.com/md2eoseo/se12-back" target="_blank" rel="noopener noreferrer">
-          Backend
-        </a>
-      </header>
-    </div>
-  );
+import MainPage from './Home';
+import Login from './Login';
+import Sign from './Sign';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route path="/Login" component={Login} />
+          <Route path="/sign" component ={Sign}/>
+          <Route path="/main" exact component={MainPage} />
+          <Route
+            render={() => (
+              <div className="error">
+                잘못된 접근입니다.
+                <Link to="/main">홈으로 돌아가기</Link>
+              </div>
+            )}
+          />
+        </Switch>
+      </Router>
+    );
+  }
 }
 
 export default App;
