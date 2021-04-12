1fbe32015a7b550ec255b19fa67c0a2ffc826b24
md2eoseo <md2eoseo@gmail.com>
2021-04-12 14:48:50 +0900

    로그인 상태에서 환영 메시지, 사용자 이름 출력

--- src/components/MyMenu.js
+++ src/components/MyMenu.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 import './css/Login.css';
 import { useHistory } from 'react-router';
-import { logUserOut } from '../client';
+import { getUserId, logUserOut } from '../client';
+import { gql, useQuery } from '@apollo/client';
+
+const GET_USER_QUERY = gql`
+  query getUser($id: Int!) {
+    getUser(id: $id) {
+      ok
+      error
+      user {
+        name
+      }
+    }
+  }
+`;
 
 function MyMenu() {
   const history = useHistory();
+  const [userId, setUserId] = useState();
+  const { data, loading } = useQuery(GET_USER_QUERY, { variables: { id: userId } });
+
+  useEffect(() => {
+    setUserId(getUserId());
+  }, []);
 
   const logout = () => {
     logUserOut();
@@ -13,6 +32,7 @@ function MyMenu() {
 
   return (
     <div className="MyMenu">
+      <div>안녕하세요! {!loading && `${data?.getUser?.user?.name}님!`}</div>
       <button className="logoutBtn" onClick={logout}>
         로그아웃
       </button>
