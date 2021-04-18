8ca82565c0caaa59745105eec6bd736da2eec1fe
md2eoseo <md2eoseo@gmail.com>
2021-04-18 15:21:46 +0900

    카테고리 목록 불러오기

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,16 +1,38 @@
+import { useQuery } from '@apollo/client';
+import gql from 'graphql-tag';
 import styled from 'styled-components';
 import Category from './Category';
 
 const Container = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: center;
+  align-items: flex-start;
+  padding: 30px 50px;
+`;
+
+const Label = styled.h2``;
+
+const SEE_CATEGORIES_QUERY = gql`
+  query seeCategories {
+    seeCategories {
+      ok
+      error
+      categories {
+        id
+        name
+      }
+    }
+  }
 `;
 
 function Categories() {
+  const { loading, data } = useQuery(SEE_CATEGORIES_QUERY);
+
   return (
     <Container>
-      <Category />
+      <Label>카테고리</Label>
+      {loading && '카테고리 불러오는 중...'}
+      {data && data.seeCategories.categories.map(category => <Category key={category.id} categoryId={category.id} name={category.name} />)}
     </Container>
   );
 }
--- src/components/Category.js
+++ src/components/Category.js
@@ -2,10 +2,11 @@ import styled from 'styled-components';
 
 const Container = styled.div`
   font-size: 20px;
+  margin: 10px 0;
 `;
 
-function Category() {
-  return <Container>category</Container>;
+function Category({ categoryId, name }) {
+  return <Container categoryId={categoryId}>{name}</Container>;
 }
 
 export default Category;
