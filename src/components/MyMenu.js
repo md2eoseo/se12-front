70d2676bd6cbc9baffeb16d6e2a9d93b69ddac90
LimitofIntegral <ufshg@naver.com>
2021-04-27 15:09:38 +0900

    헤더에 장바구니 버튼 구현(임시)

--- src/components/MyMenu.js
+++ src/components/MyMenu.js
@@ -4,15 +4,47 @@ import { getUserId, logUserOut } from '../client';
 import { gql, useQuery } from '@apollo/client';
 import styled from 'styled-components';
 
+const AppBlock = styled.div`
+  width: 200px;
+  margin: 0 auto;
+  margin-top: 1rem;
+  border: 1px solid black;
+  padding: 1rem;
+`;
+
 const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
 `;
 
-const Greeting = styled.div``;
+const Greeting = styled.div`
+  margin-bottom: 10px;
+`;
 
-const LogoutBtn = styled.button``;
+const LogoutBtn = styled.button`
+  width: 64px;
+  height: 30px;
+  color: white;
+  background-color: gray;
+  font-size: 100%;
+  cursor: pointer;
+  outline: none;
+  border: none;
+  margin-left: 16px;
+`;
+
+const BagBtn = styled.button`
+  width: 64px;
+  height: 30px;
+  color: white;
+  background-color: cornflowerblue;
+  font-size: 100%;
+  cursor: pointer;
+  outline: none;
+  border: none;
+  margin-right: 16px;
+`;
 
 const GET_USER_QUERY = gql`
   query getUser($id: Int!) {
@@ -42,8 +74,11 @@ function MyMenu() {
 
   return (
     <Container>
-      <Greeting>안녕하세요! {!loading && `${data?.getUser?.user?.name}님!`}</Greeting>
-      <LogoutBtn onClick={logout}>로그아웃</LogoutBtn>
+      <AppBlock>
+        <Greeting>안녕하세요! {!loading && `${data?.getUser?.user?.name}님!`}</Greeting>
+        <BagBtn onClick={logout}>장바구니</BagBtn>
+        <LogoutBtn onClick={logout}>로그아웃</LogoutBtn>
+      </AppBlock>
     </Container>
   );
 }
