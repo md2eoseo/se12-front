400ba74b87eed075179d9cdc590ff7d8decb4be9
Haesol Choi <chsgood1004@gmail.com>
2021-05-11 17:01:00 +0900

    카테고리 버튼 구현

--- src/components/Category.js
+++ src/components/Category.js
@@ -4,9 +4,19 @@ const Container = styled.div`
   font-size: 20px;
   margin: 10px 0;
 `;
+const Button = styled.button`
+  width: 100px;
+  height: 40px;
+  border: 0;
+  margin-bottom: 40px;
+  background-color: #487be1;
+  color: #d9e5ff;
+  font-size: 18px;
+  cursor: pointer;
+`;
 
 function Category({ categoryId, name }) {
-  return <Container categoryId={categoryId}>{name}</Container>;
+  return <Button categoryId={categoryId}>{name}</Button>;
 }
 
 export default Category;
