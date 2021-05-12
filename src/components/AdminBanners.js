d85ab95f3e1e63680c87fe46ab398b144093a3d1
Haesol Choi <chsgood1004@gmail.com>
2021-05-12 17:15:45 +0900

    배너관리 저장버튼

--- src/components/AdminBanners.js
+++ src/components/AdminBanners.js
@@ -14,6 +14,24 @@ const Label = styled.h2`
   padding: 20px;
 `;
 
+const Button = styled.div`
+  padding: 0px 0px 20px 700px;
+`;
+
+const SaveButton = styled.button`
+  width: 110px;
+  height: 50px;
+  font-size: 18px;
+  color: white;
+  background-color: #487be1;
+  cursor: pointer;
+  outline: none;
+  border: none;
+  &:hover {
+    background-color: cornflowerblue;
+  }
+`;
+
 const SEE_ALL_BANNERS_QUERY = gql`
   query seeAllBanners {
     seeAllBanners {
@@ -34,10 +52,16 @@ const SEE_ALL_BANNERS_QUERY = gql`
 
 function AdminBanners() {
   const { loading, data } = useQuery(SEE_ALL_BANNERS_QUERY);
-
+  function refreshPage() {
+    window.location.reload();
+  }
   return (
     <Container>
       <Label>공지/이벤트 관리</Label>
+      <Button>
+        <SaveButton onClick={refreshPage}>저장</SaveButton>
+      </Button>
+
       {loading && '배너 불러오는 중...'}
       {data &&
         data.seeAllBanners.banners.map(banner => (
