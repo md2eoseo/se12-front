88a6945b6a67d5f065221952f8f42cd3e7226e85
Haesol Choi <chsgood1004@gmail.com>
2021-05-12 15:56:38 +0900

    메인화면 카테고리 디자인 수정

--- src/components/Category.js
+++ src/components/Category.js
@@ -2,14 +2,16 @@ import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 const Button = styled.button`
-  width: 100px;
-  height: 40px;
+  width: 250px;
+  height: 70px;
   border: 0;
-  margin-bottom: 40px;
   background-color: #487be1;
   color: #d9e5ff;
-  font-size: 18px;
+  font-size: 20px;
   cursor: pointer;
+  &:hover {
+    background-color: cornflowerblue;
+  }
 `;
 
 function Category({ categoryId, name }) {
