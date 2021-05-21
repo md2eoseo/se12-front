9959b600c754bc2bb617ddb21d3aa21a6171cfb8
Haesol Choi <chsgood1004@gmail.com>
2021-05-21 19:33:48 +0900

    itempage

--- src/routes/ItemPage.js
+++ src/routes/ItemPage.js
@@ -2,7 +2,7 @@ import { gql, useQuery } from '@apollo/client';
 import styled from 'styled-components';
 
 function ItemPage() {
-  return <p>Hello</p>;
+  return <p>상세페이지입니다</p>;
 }
 
 export default ItemPage;
