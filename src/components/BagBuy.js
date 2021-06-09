9a9168256bcaa5a036e8c983093fa35869803ee9
NyaNyak <chsgood8182@naver.com>
2021-06-09 18:29:29 +0900

    구매한도 메시지 추가

--- src/components/BagBuy.js
+++ src/components/BagBuy.js
@@ -131,7 +131,6 @@ function BagBuy() {
   const { loading, data, refetch } = useQuery(SEE_BAG_QUERY);
   const maxShippingFee = Number(localStorage.getItem('Fee'));
   const FREE_SHIPPING_LIMIT = 20000;
-  const payment = totalPrice + maxShippingFee;
 
   useEffect(() => {
     if (data?.seeBag) {
@@ -143,31 +142,35 @@ function BagBuy() {
   }, [data]);
 
   async function showKakaoPay() {
-    const instance = axios.create({
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8',
-        Authorization: `KakaoAK ${process.env.REACT_APP_KAKAO_AK}`,
-      },
-    });
-    await instance
-      .post(
-        '/v1/payment/ready',
-        querystring.stringify({
-          cid: 'TC0ONETIME',
-          partner_order_id: '00000001',
-          partner_user_id: getUserId(),
-          item_name: '장바구니 상품',
-          quantity: totalQuantity,
-          total_amount: totalPrice < 20000 ? totalPrice + maxShippingFee : totalPrice,
-          tax_free_amount: totalPrice < 20000 ? totalPrice + maxShippingFee : totalPrice,
-          approval_url: `${process.env.REACT_APP_BASEURL}/pay/success`,
-          cancel_url: `${process.env.REACT_APP_BASEURL}/pay/cancel`,
-          fail_url: `${process.env.REACT_APP_BASEURL}/pay/fail`,
-        })
-      )
-      .then(res => {
-        window.location.replace(res.data.next_redirect_pc_url);
+    if (totalPrice > 1000000) {
+      window.alert('한번에 결제 가능한 가격은 최대 100만원입니다.');
+    } else {
+      const instance = axios.create({
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8',
+          Authorization: `KakaoAK ${process.env.REACT_APP_KAKAO_AK}`,
+        },
       });
+      await instance
+        .post(
+          '/v1/payment/ready',
+          querystring.stringify({
+            cid: 'TC0ONETIME',
+            partner_order_id: '00000001',
+            partner_user_id: getUserId(),
+            item_name: '장바구니 상품',
+            quantity: totalQuantity,
+            total_amount: totalPrice < 20000 ? totalPrice + maxShippingFee : totalPrice,
+            tax_free_amount: totalPrice < 20000 ? totalPrice + maxShippingFee : totalPrice,
+            approval_url: `${process.env.REACT_APP_BASEURL}/pay/success`,
+            cancel_url: `${process.env.REACT_APP_BASEURL}/pay/cancel`,
+            fail_url: `${process.env.REACT_APP_BASEURL}/pay/fail`,
+          })
+        )
+        .then(res => {
+          window.location.replace(res.data.next_redirect_pc_url);
+        });
+    }
   }
 
   return (
